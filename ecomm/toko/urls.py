4d4336e460ba741b1a87ab6536febabe9feef3b2
sssaal <salsabil.zain@gmail.com>
2023-06-01 17:46:41 +0700

    init project

---
+++ ecomm/toko/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+app_name = 'toko'
+
+urlpatterns = [
+     path('', views.HomeListView.as_view(), name='home-produk-list'),
+     path('product/<slug>/', views.ProductDetailView.as_view(), name='produk-detail'),
+     path('checkout/', views.CheckoutView.as_view(), name='checkout'),
+     path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
+     path('remove_from_cart/<slug>/', views.remove_from_cart, name='remove-from-cart'),
+     path('order-summary/', views.OrderSummaryView.as_view(), name='order-summary'),
+     path('payment/<payment_method>', views.PaymentView.as_view(), name='payment'),
+     path('paypal-return/', views.paypal_return, name='paypal-return'),
+     path('paypal-cancel/', views.paypal_cancel, name='paypal-cancel'),
+     path('search/', views.SearchListView.as_view(), name='search-results'), 
+     path('remove_from_order/<slug>/', views.remove_single_item_order_summary, name='remove-from-order'),
+     path('add_from_order/<slug>/', views.add_item_order_summary, name='add-from-order'),
+
+]
